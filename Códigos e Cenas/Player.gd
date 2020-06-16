cdc01021a9ec6cc7baef43f7120cae38a41e20f7
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2020-06-16 00:12:46 -0300

    Player,Rotação e Tp

---
+++ Códigos e Cenas/Player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+export (int) var CoolDownTP = 3
+var can_TP = true
+
+func _process(_delta):
+	var look_vec = get_global_mouse_position() - global_position
+	global_rotation = atan2(look_vec.y, look_vec.x)
+	
+	if Input.is_action_just_pressed("Teleport") and can_TP:
+		position = get_global_mouse_position()
+		can_TP = false
+		yield(get_tree().create_timer(CoolDownTP),"timeout")
+		can_TP = true
+		
