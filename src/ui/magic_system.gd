0a4d9973e26e382b701f8549e71453f7895e3a16
MasterProjectLC <dan.inafuku@hotmail.com>
2020-06-17 08:16:46 -0300

    magic system com novos signals pro anderson

--- src/ui/magic_system.gd
+++ src/ui/magic_system.gd
@@ -1,18 +1,29 @@
 extends Node
 
-var keys = [KEY_Q, KEY_W, KEY_E, KEY_R, 
-			KEY_A, KEY_S, KEY_D, KEY_F,
-			KEY_Z, KEY_X, KEY_C, KEY_V]
+# ---------------------
 
-var spells = ["fireball", "laser", "sword", "seilamanokk"]
+# parametros de signal reset_spells: nada nada nada, nada
+# parametros de signal cast_spell: (packed_scene do spell, letra do spell, posicao do spell na tela)
+
+# ---------------------
+
+
+var keys = [KEY_Q, KEY_W, KEY_E, KEY_R, 
+			KEY_A, KEY_S, KEY_D, KEY_F]
+
+export var spells = [[], [], [], []]
+# damage, knockback, stun/break, cura/escudo
 
 # key da magia: magia
-var active_spells = {KEY_Q:"fireball"}
-var spells_at_a_time = 4
+# e.g: KEY_Q: spells[0]
 
-var scroll_count = 10
-var max_scroll_count = 10
+var active_spells = {}
+export var spells_at_a_time = 4
 
+export var max_scroll_count = 10
+onready var scroll_count = max_scroll_count
+
+signal reset_spells
 signal cast_spell
 
 func _ready():
@@ -23,12 +34,12 @@ func _input(event):
 		var keycode = event.scancode
 		
 		if keycode in active_spells.keys():
-			#emit_signal("cast_spell", active_spells[keycode])
-			cast_spell(active_spells[keycode])
+			cast_spell(keycode)
 
 
-func cast_spell(spell):
-	call_deferred(spell)
+func cast_spell(keycode):
+	emit_signal("cast_spell", active_spells[keycode], OS.get_scancode_string(keycode), 
+				active_spells.keys().find(keycode))
 	
 	# Desce a quantidade de casts disponiveis
 	scroll_count -= 1
@@ -36,29 +47,27 @@ func cast_spell(spell):
 		scroll_count = max_scroll_count
 		reset_spells()
 
+
 func reset_spells():
 	active_spells.clear()
+	emit_signal("reset_spells")
+	var key_position = -1
 	
 	for i in range(spells_at_a_time):
-		# generate a new random key
-		var rand_key = keys[randi() % len(keys)]
-		while rand_key in active_spells.keys():
-			rand_key = keys[randi() % len(keys)]
+		# generate the next random key
+		var remaining_options = len(keys) - key_position - (spells_at_a_time - i)
+		var rand_key_offset = 1
+		if remaining_options != 0:
+			rand_key_offset = 1 + (randi() % remaining_options)
+		
+		key_position += rand_key_offset
+		var rand_key = keys[key_position]
 		
 		# generate a new random spell
-		var rand_spell = spells[randi() % len(spells)]
+		var rand_spell = spells[i][randi() % len(spells[i])]
 		while rand_spell in active_spells.values():
-			rand_spell = spells[randi() % len(spells)]
+			rand_spell = spells[i][randi() % len(spells[i])]
 		
 		active_spells[rand_key] = rand_spell
-
-
-# FUNCOES DOS SPELLS -------------
-func fireball():
-	print("fireball")
-	# funcao da magia aqui
-
-func laser():
-	print("LASER VRAUUU")
-	# funcao da magia aqui
-
+		
+		print_debug(str(i) + ": " + OS.get_scancode_string(rand_key) + ", " + rand_spell.name)
