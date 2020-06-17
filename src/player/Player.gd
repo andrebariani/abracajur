23795c987228567a068117de395f0fd610882151
andrebariani <30839954+andrebariani@users.noreply.github.com>
2020-06-17 14:05:28 -0300

    SpellLauncher and changes to scenes (#7)
    * commiting for merge
    
    * Spell launcher and changes to LaserSpell

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -3,20 +3,29 @@ extends KinematicBody2D
 onready var pointer = $Pointer
 onready var rayCast = $RayCast2D
 onready var anim = $LampAnim
+onready var spellScene = preload("res://src/spells/SparkSpell.tscn")
+
 
 export (int) var cooldownTeleport = 0.1
+
+
 var can_teleport = true
+var look_vector = Vector2.ZERO
 
 
 func _process(_delta):
-	var look_vec = get_global_mouse_position() - global_position
-	pointer.rotation = atan2(look_vec.y, look_vec.x)
+	look_vector = get_global_mouse_position() - global_position
+	pointer.rotation = atan2(look_vector.y, look_vector.x)
 	
 	if Input.is_action_just_pressed("ui_select") and can_teleport:
-		rayCast.cast_to = look_vec
+		rayCast.cast_to = look_vector
 		rayCast.force_raycast_update()
 		if !rayCast.is_colliding():
 			teleport()
+			
+			
+	if Input.is_action_just_pressed("spell_q"):
+		_on_cast_spell(spellScene)
 
 
 func teleport():
@@ -26,3 +35,14 @@ func teleport():
 	yield(get_tree().create_timer(cooldownTeleport),"timeout")
 	can_teleport = true
 	
+	
+func _on_cast_spell(spell_scene):
+	var spell = spell_scene.instance()
+	var player_radius = get_node("Hurtbox/CollisionShape2D").get_shape().radius
+	var spell_radius = spell.get_node("Hitbox/CollisionShape2D").get_shape().radius
+	var total_radius = player_radius + spell_radius
+	spell.position = global_position + Vector2(total_radius, total_radius) * look_vector.normalized()
+	spell.init(look_vector.normalized())
+	
+	var world = get_tree().current_scene
+	world.add_child(spell)
