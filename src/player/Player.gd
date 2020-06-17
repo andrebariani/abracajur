ae699875671ba6234b8da67dd281827e36e34d74
andrebariani <andrebariani@gmail.com>
2020-06-17 10:08:30 -0300

    some code polish

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -7,16 +7,16 @@ onready var anim = $LampAnim
 export (int) var cooldownTeleport = 0.1
 var can_teleport = true
 
+
 func _process(_delta):
 	var look_vec = get_global_mouse_position() - global_position
-	$Pointer.rotation = atan2(look_vec.y, look_vec.x)
+	pointer.rotation = atan2(look_vec.y, look_vec.x)
 	
 	if Input.is_action_just_pressed("ui_select") and can_teleport:
 		rayCast.cast_to = look_vec
 		rayCast.force_raycast_update()
 		if !rayCast.is_colliding():
 			teleport()
-		
 
 
 func teleport():
