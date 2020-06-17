0d48d0b46cf339339d576caf3ab640a017769738
andrebariani <andrebariani@gmail.com>
2020-06-16 23:35:32 -0300

    player teleport not going through walls

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,15 +1,28 @@
 extends KinematicBody2D
 
-export (int) var CoolDownTP = 3
-var can_TP = true
+onready var pointer = $Pointer
+onready var rayCast = $RayCast2D
+onready var anim = $LampAnim
+
+export (int) var cooldownTeleport = 0.1
+var can_teleport = true
 
 func _process(_delta):
 	var look_vec = get_global_mouse_position() - global_position
-	global_rotation = atan2(look_vec.y, look_vec.x)
+	$Pointer.rotation = atan2(look_vec.y, look_vec.x)
 	
-	if Input.is_action_just_pressed("ui_cancel") and can_TP:
-		position = get_global_mouse_position()
-		can_TP = false
-		yield(get_tree().create_timer(CoolDownTP),"timeout")
-		can_TP = true
+	if Input.is_action_just_pressed("ui_select") and can_teleport:
+		rayCast.cast_to = look_vec
+		rayCast.force_raycast_update()
+		if !rayCast.is_colliding():
+			teleport()
 		
+
+
+func teleport():
+	anim.play("teleport")
+	global_position = get_global_mouse_position()
+	can_teleport = false
+	yield(get_tree().create_timer(cooldownTeleport),"timeout")
+	can_teleport = true
+	
