14c1b8489b67b583d6c70a4ecef5b157643c7601
andrebariani <andrebariani@gmail.com>
2020-06-18 20:55:57 -0300

    All effects done

--- src/enemies/Enemy.gd
+++ src/enemies/Enemy.gd
@@ -19,31 +19,37 @@ onready var hp = max_hp
 onready var AggroBox = $AggroBox
 onready var rayCast = $RayCast2D
 onready var moveDebug = $MoveDebug
+onready var shieldTimer = $ShieldTimer
 
 enum {
-	WANDER
+	IDLE
 	CHASE
 	STUN
 }
 
-var state = WANDER
+var state = IDLE
 
 var MoveDirection = Vector2.ZERO
 var knockback = Vector2.ZERO
+var has_shield = false setget set_shield
+
+func set_shield(value):
+	has_shield = value
 
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, FRICTION)
-	knockback = move_and_slide(knockback)
-	
 	state_machine()
-	MoveDirection = move_and_slide(MoveDirection)
-	moveDebug.set_text(str(MoveDirection))
+	if knockback != Vector2.ZERO:
+		knockback = knockback.move_toward(Vector2.ZERO, FRICTION)
+		knockback = move_and_slide(knockback)
+	else:
+		MoveDirection = move_and_slide(MoveDirection)
+	moveDebug.set_text(str(MoveDirection) + str(state))
 
 
 func state_machine():
 	match state:
-		WANDER:
+		IDLE:
 			seek_player()
 			MoveDirection = MoveDirection.move_toward(Vector2.ZERO,FRICTION)
 		CHASE:
@@ -55,14 +61,16 @@ func state_machine():
 				if !rayCast.is_colliding():
 					MoveDirection = MoveDirection.move_toward(direction.normalized() * MAX_SPEED, ACCELERATION)
 				else:
-					set_state(WANDER)
+					set_state(IDLE)
 			else:
-				set_state(WANDER)
+				set_state(IDLE)
 		STUN:
+			MoveDirection = MoveDirection.move_toward(Vector2.ZERO,FRICTION)
 			return
 
 func set_state(new_state):
-	state = new_state
+	if new_state != state:
+		state = new_state
 
 func seek_player():
 	if AggroBox.can_see():
@@ -72,28 +80,31 @@ func seek_player():
 # ---- Handle signals ----------
 
 func _on_Hurtbox_area_entered(area):
-	var spell = area.spell
-	match spell.chosen_effect:
-		"DAMAGE":
-			apply_damage(spell.effects)
-		"STUN":
-			apply_stun(spell.effects)
-		"KNOCKBACK":
-			apply_knockback(area)
-		"GREASE":
-			apply_grease(spell.effects)
-		"BREAK":
-			apply_break(spell.effects)
-		"HEAL":
-			apply_heal(spell.effects)
-		"SHIELD":
-			apply_shield(spell.effects)
+	if not has_shield:
+		var spell = area.spell
+		match spell.chosen_effect:
+			"DAMAGE":
+				apply_damage(spell.effects)
+			"STUN":
+				apply_stun(spell.effects)
+			"KNOCKBACK":
+				apply_knockback(area)
+			"GREASE":
+				apply_grease(spell.effects)
+			"BREAK":
+				apply_break(spell.effects)
+			"HEAL":
+				apply_heal(spell.effects)
+			"SHIELD":
+					apply_shield(spell.effects)
 	
 	
 # ---- React to stimuli -------------
 
 func apply_damage(spell_effects):
 	hp = clamp(hp - spell_effects.DAMAGE, 0, max_hp)
+	if hp == 0:
+		die()
 	print_debug("damage! " + str(hp))
 
 
@@ -105,12 +116,12 @@ func apply_heal(spell_effects):
 func apply_stun(spell_effects):
 	set_state(STUN)
 	$StunTimer.start(spell_effects.STUN)
+	print_debug("ouch!")
 	
 
 func apply_knockback(area):
 	var knockback_direction = area.global_position.direction_to(self.global_position)
 	knockback = area.spell.effects.KNOCKBACK * knockback_direction.normalized()
-	print(str(knockback))
 
 
 func apply_grease(spell_effects):
@@ -124,8 +135,19 @@ func apply_break(spell_effects):
 	
 
 func apply_shield(spell_effects):
-	hp += spell_effects.DAMAGE
-	print_debug(hp)
+	has_shield = true
+	shieldTimer.start(spell_effects.SHIELD)
+	print_debug("Shield is on!")
+
 
 func _on_StunTimer_timeout():
-	set_state(WANDER)
+	set_state(IDLE)
+
+
+func _on_ShieldTimer_timeout():
+	has_shield = false
+	print_debug("Shield is out")
+
+
+func die():
+	queue_free()
--- src/player/Player.gd
+++ src/player/Player.gd
@@ -4,32 +4,38 @@ onready var pointer = $Pointer
 onready var rayCast = $RayCast2D
 onready var anim = $LampAnim
 onready var hurtbox = $Hurtbox/CollisionShape2D
-
+onready var stunTimer = $StunTimer
+onready var shieldTimer = $ShieldTimer
 
 export (int) var cooldownTeleport = 0.1
+export var max_hp = 3
 
-
+onready var hp = max_hp
 var can_teleport = true
 var look_vector = Vector2.ZERO
+var is_active = true
+var has_shield = false
 
+signal player_stunned
 
 func _process(_delta):
 	look_vector = get_global_mouse_position() - global_position
 	pointer.rotation = atan2(look_vector.y, look_vector.x)
 	
-	if Input.is_action_just_pressed("ui_select") and can_teleport:
-		rayCast.cast_to = look_vector
-		rayCast.force_raycast_update()
-		
-		anim.play("teleport")
-		if !rayCast.is_colliding():
-			teleport_to_mouse()
-		else:
-			teleport_to_nearest_wall()
+	if is_active:
+		if Input.is_action_just_pressed("ui_select") and can_teleport:
+			rayCast.cast_to = look_vector
+			rayCast.force_raycast_update()
 			
-		can_teleport = false
-		yield(get_tree().create_timer(cooldownTeleport),"timeout")
-		can_teleport = true
+			anim.play("teleport")
+			if !rayCast.is_colliding():
+				teleport_to_mouse()
+			else:
+				teleport_to_nearest_wall()
+				
+			can_teleport = false
+			yield(get_tree().create_timer(cooldownTeleport),"timeout")
+			can_teleport = true
 
 
 func teleport_to_mouse():
@@ -57,3 +63,62 @@ func _on_Magic_System_cast_spell(spell_data, letter, position):
 	
 	var world = get_tree().current_scene
 	world.add_child(spell)
+
+
+func _on_Hurtbox_area_entered(area):
+	if not has_shield:
+		var spell = area.spell
+		match spell.chosen_effect:
+			"DAMAGE":
+				apply_damage(spell.effects)
+			"STUN":
+				apply_stun(spell.effects)
+			"BREAK":
+				apply_break(spell.effects)
+			"HEAL":
+				apply_heal(spell.effects)
+			"SHIELD":
+				apply_shield(spell.effects)
+	
+	
+# ---- React to stimuli -------------
+
+func apply_damage(spell_effects):
+	hp = clamp(hp - spell_effects.DAMAGE, 0, max_hp)
+	if hp == 0:
+		die()
+	print_debug("damage! " + str(hp))
+
+
+func apply_heal(spell_effects):
+	hp = clamp(hp + spell_effects.HEAL, 0, max_hp)
+	print_debug("heal! " + str(hp))
+
+
+func apply_stun(spell_effects):
+	is_active = false
+	emit_signal("player_stunned", is_active)
+	stunTimer.start(spell_effects.STUN)
+	
+	
+func apply_break(spell_effects):
+	max_hp = max(max_hp - spell_effects.BREAK, 1)
+	print_debug("HP reduced to " + str(max_hp))
+	
+
+func apply_shield(spell_effects):
+	has_shield = true
+
+
+func _on_StunTimer_timeout():
+	is_active = true
+	emit_signal("player_stunned", is_active)
+
+
+func _on_ShieldTimer_timeout():
+	has_shield = false
+	print_debug("Shield is out")
+
+
+func die():
+	queue_free()
