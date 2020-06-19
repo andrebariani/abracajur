905053b53d9671ac41107a319bfe74cf1e2b6340
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2020-06-19 18:45:47 -0300

    a

--- src/engine/Hurtbox.gd
+++ src/engine/Hurtbox.gd
@@ -1,36 +1,6 @@
 extends Area2D
 
-<<<<<<< HEAD
-onready var timer = $Timer 
 
-var invincible = false setget set_invincible
-
-signal invicibility_started
-signal invicibilty_ended
-
-func set_invincible(value):
-	invincible = value
-	if invincible == true:
-		emit_signal("invicibility_started")
-	else:
-		emit_signal("invicibilty_ended")
-
-func start_invicibility(duration):
-	self.invincible = true
-	timer.start(duration)
-
-
-func _on_Timer_timeout():
-	self.invincible = false
-
-
-func _on_Hurtbox_invicibility_started():
-	set_deferred("monitorable",false)
-
-
-func _on_Hurtbox_invicibilty_ended():
-	set_deferred("monitorable",true)
-=======
 var invincibility_active = false setget set_invincibility
 
 signal invincibility_started
@@ -62,4 +32,3 @@ func _on_Hurtbox_invincibility_started():
 func _on_Hurtbox_invincibility_ended():
 	$CollisionShape2D.disabled = false
 	print_debug("inv off.")
->>>>>>> bariani
