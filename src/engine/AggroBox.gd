8d5cf5fe18dc699eb861acc5c46ca281b9333a14
andrebariani <30839954+andrebariani@users.noreply.github.com>
2020-06-20 14:54:44 -0300

    Danilo (#23)
    * fixes gerais e runa ativando com o player
    
    * vc quer mais fix? vai ter mais fix
    
    * rune fix
    
    * mais icones
    
    * ilusao com menos lag
    
    * illusion fix
    
    * Botao-Rajada
    
    Co-authored-by: MasterProjectLC <dan.inafuku@hotmail.com>
    Co-authored-by: ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>

--- src/engine/AggroBox.gd
+++ src/engine/AggroBox.gd
@@ -9,10 +9,14 @@ func _on_AggroBox_body_entered(body):
 	if can_see():
 		return
 	target = body
+	
+	target.get_signal_list()
 	target.connect("activated_illusion", get_parent(), "get_diverted")
 	$CollisionShape2D.self_modulate = Color(1, 1, 1, 0.5)
 
 
 func _on_AggroBox_body_exited(_body):
-	target = null
+	if is_instance_valid(target) and target.is_connected("activated_illusion", get_parent(), "get_diverted"):
+		target.disconnect("activated_illusion", get_parent(), "get_diverted")
+		target = null
 	$CollisionShape2D.self_modulate = Color(1, 1, 1, 0.1)
