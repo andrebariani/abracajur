59d032de6c692ae89058312539bf450ed9a7fada
andrebariani <andrebariani@gmail.com>
2020-06-19 23:03:40 -0300

    Spell Tooltip added

--- src/ui/UI.gd
+++ src/ui/UI.gd
@@ -1,8 +1,12 @@
 extends Control
 
+onready var tooltip = $Tooltip
+onready var tooltipAnim = $TooltipAnim
 
 func _on_MagicSystem_cast_spell(spell, letter, position):
 	get_node("HBoxContainer/Spell" + letter).setup(spell)
+	tooltip.set_text(spell["NAME"])
+	tooltipAnim.play("start")
 
 
 func _on_MagicSystem_reset_spells():
